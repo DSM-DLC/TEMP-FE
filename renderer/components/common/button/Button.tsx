a89e77dfcaf6b740b4f30dc1946498322a3bc244
ensnif060305 <jmsun0305@gmail.com>
2023-09-22 18:38:51 +0900

    Docs: Add parameters and modify styles

--- renderer/components/common/button/Button.tsx
+++ renderer/components/common/button/Button.tsx
@@ -6,42 +6,46 @@ interface buttonType extends HtmlHTMLAttributes<HTMLButtonElement> {
     onClick?: () => void
     label?: string | number
     justifyContent?: string
-    width?: number
-    height?: number
+    width?: string
+    height?: string
     margin?: string
     border?: string
+    borderRadius?: string
     backColor?: string
-    color?: string
+    fontSize?: string
+    padding?: string
+    textAlign?: string
 }
 
-export const Button = ({ label, ...props }: buttonType) => {
+export const Button = ({ label, justifyContent, ...props }: buttonType) => {
     return (
-        <ButtonInner>
+        <ButtonInner justifyContent={justifyContent}>
             <ButtonBox {...props}>{label}</ButtonBox>
         </ButtonInner>
     )
 }
 
-const ButtonInner = styled.div`
+const ButtonInner = styled.div<{ justifyContent?: string }>`
     width: auto;
     height: auto;
     display: flex;
-    justify-content: center;
+    justify-content: "center";
 `
 
 const ButtonBox = styled.button<buttonType>`
-    margin: ${props => props.margin ?? "2px 2px 2px 2px"};
-    padding: 15px;
+    vertical-align: middle;
+    padding: ${props => props.padding ?? "15px"};
+    margin: ${props => props.margin ?? "40px 0 0 0"};
     border: ${props => props.border ?? "none"};
     width: ${props => props.width ?? "15vw"};
     height: ${props => props.height ?? "4vh"};
-    margin: ${props => props.margin ?? "40px 0 0 0"};
-    background: ${({ theme }) => theme.color.blue50};
-    border-radius: 10px;
-    color: ${({ theme }) => theme.color.blue500};
+    background: ${({ theme }) => theme.color.blue400};
+    border-radius: ${props => props.borderRadius ?? "10px"};
+    color: ${({ theme }) => theme.color.white};
     font-family: Gmarket Sans;
-    font-size: 11px;
+    font-size: ${props => props.fontSize ?? "11px"};
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    text-align: ${props => props.textAlign ?? "center"};
 `
